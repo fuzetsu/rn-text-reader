ff6d811a997b7768d462be9be77a1a1f58679f20
Daniel Loomer <fuzetsu@users.noreply.github.com>
2021-04-24 17:25:00 -0400

    refactor ref updating logic into hook

--- lib/hooks.ts
+++ lib/hooks.ts
@@ -11,10 +11,18 @@ export const useKeepAwake = (keepAwake: boolean) => {
   }, [keepAwake])
 }
 
+export const useUpdatingRef = <T>(value: T) => {
+  const ref = useRef(value)
+  ref.current = value
+  return ref
+}
+
 export const useDoublePress = <T extends () => void>(fn: T) => {
+  const fnRef = useUpdatingRef(fn)
+
   const lastPress = useRef(0)
   return useCallback(() => {
-    if (Date.now() - lastPress.current < 500) fn()
+    if (Date.now() - lastPress.current < 500) fnRef.current()
     lastPress.current = Date.now()
   }, [])
 }
