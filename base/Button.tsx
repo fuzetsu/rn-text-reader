61ddca07d312d5a3c385a11f0097a7777a5992ad
Daniel Loomer <fuzetsu@users.noreply.github.com>
2021-04-28 18:53:52 -0400

    support plain button style prop

--- base/Button.tsx
+++ base/Button.tsx
@@ -5,30 +5,38 @@ interface Props extends ComponentProps<typeof TouchableOpacity> {
   text: string
   textStyle?: StyleProp<TextStyle>
   primary?: boolean
+  plain?: boolean
 }
 
-export const Button = ({ text, style, textStyle, disabled, primary, ...props }: Props) => (
+export const Button = ({ text, style, textStyle, plain, disabled, primary, ...props }: Props) => (
   <TouchableOpacity
     {...props}
     disabled={disabled}
-    style={[styles.button, disabled && styles.disabled, primary && styles.primary, style]}
+    style={[
+      styles.common,
+      plain ? styles.plain : styles.default,
+      disabled && styles.disabled,
+      primary && styles.primary,
+      style,
+    ]}
   >
-    <Text style={[styles.buttonText, disabled && styles.disabledText, textStyle]}>{text}</Text>
+    <Text style={[styles.text, disabled && styles.disabledText, textStyle]}>{text}</Text>
   </TouchableOpacity>
 )
 
 const styles = StyleSheet.create({
-  primary: { backgroundColor: 'rgb(70, 48, 235)' },
-  buttonText: { fontSize: 16, color: 'white' },
+  text: { fontSize: 16, color: 'white' },
   disabled: { backgroundColor: '#333' },
   disabledText: { color: '#777' },
-  button: {
-    height: 39,
+  common: {
+    minHeight: 39,
     margin: 2,
     borderRadius: 4,
     paddingHorizontal: 15,
-    backgroundColor: '#555',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  primary: { backgroundColor: 'rgb(70, 48, 235)' },
+  plain: { borderColor: 'white', borderWidth: 1 },
+  default: { backgroundColor: '#555' },
 })
