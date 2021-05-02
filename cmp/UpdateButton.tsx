a99f4e18ece86c598789ae2cc0c104afbbe241fe
Daniel Loomer <fuzetsu@users.noreply.github.com>
2021-05-02 01:18:40 -0400

    fetch update when update btn is clicked

--- cmp/UpdateButton.tsx
+++ cmp/UpdateButton.tsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react'
 import * as Updates from 'expo-updates'
+import { Alert } from 'react-native'
 import { Button } from '../base'
 
 export function UpdateButton() {
@@ -14,7 +15,13 @@ export function UpdateButton() {
 
   if (!hasUpdate) return null
 
-  const update = () => Updates.reloadAsync()
+  const update = async () => {
+    const { isNew } = await Updates.fetchUpdateAsync()
+    if (isNew) await Updates.reloadAsync()
+    else Alert.alert('Never mind', 'Turns out app was already up to date')
+  }
 
-  return <Button text="New update, tap to reload" icon={{ name: 'update' }} onPress={update} />
+  return (
+    <Button primary text="New update, tap to reload" icon={{ name: 'update' }} onPress={update} />
+  )
 }
